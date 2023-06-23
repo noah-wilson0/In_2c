b14c2abd6fbf9260a113f36e0c286d0f85f777f5
Noah-wilson <zeus01pop@gmail.com>
2023-06-23 13:53:34 +0900

    java gui test

--- src/Frame.java
+++ src/Frame.java
@@ -8,11 +8,22 @@ public class Frame {
 
         JFrame frame =new JFrame();
         JPanel panel=new JPanel();
+        JLabel label=new JLabel("some test");
+        JButton btn1=new JButton("click me:!");
+        JTextArea textArea=new JTextArea();
+        JTextField textField=new JTextField(100);
+
+        panel.setLayout(new BorderLayout());
+
+//        panel.add(new JLabel("first JLabel"));
+
+        panel.setLayout(new BorderLayout());
+        panel.add(label,BorderLayout.NORTH);
+        panel.add(btn1,BorderLayout.WEST);
+        panel.add(textArea,BorderLayout.CENTER);
+
 
         frame.add(panel);
-
-        panel.add(new JLabel("first JLabel"));
-
         frame.setResizable(false);
         frame.setVisible(true);
         frame.setPreferredSize(new Dimension(840,840/12*9));
