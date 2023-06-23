69ac985bff5ea3f96f89863137f547c3ba575ce6
Noah-wilson <zeus01pop@gmail.com>
2023-06-23 12:21:35 +0900

    java gui test

---
+++ src/Frame.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+import java.security.KeyStore;
+
+public class Frame {
+    public static void main(String[] args) {
+
+
+        JFrame frme =new JFrame();
+
+        frme.setVisible(true);
+        frme.setPreferredSize(new Dimension(840,840/12*9));
+
+
+    }
+}
