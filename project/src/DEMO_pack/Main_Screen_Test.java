19a89f351c476be53802b172a542333d5a8e2f74
noah-wilson0 <127368908+noah-wilson0@users.noreply.github.com>
2023-12-07 00:08:58 +0900

    Delete project directory

--- project/src/DEMO_pack/Main_Screen_Test.java
+++
@@ -1,8 +0,0 @@
-package DEMO_pack;
-
-//public class Main_Screen_Test {
-//    public static void main(String[] args) {
-//        MainFrame mf = MainFrame.getInstance();
-//        mf.ChangeGUI("메인화면");
-//    }
-//}
